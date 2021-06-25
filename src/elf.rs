5938950c093862e2ae447e15c178e9674275b862
tux3 <barrdetwix@gmail.com>
2021-06-25 21:11:37 +0200

    Demoting COMDAT section groups into regular groups
    This is useful to prevent a symbol in those section groups from
    merging with potentially incompatible COMDATs in other object files.

--- src/elf.rs
+++ src/elf.rs
@@ -9,10 +9,15 @@ use goblin::elf::section_header::{
     section_header32, section_header64, SHT_GNU_HASH, SHT_GNU_VERSYM, SHT_HASH, SHT_NULL, SHT_RELA,
     SHT_SYMTAB_SHNDX,
 };
-use goblin::elf::{Elf, Header, SectionHeaders};
+use goblin::elf::{Elf, Header, SectionHeaders, Sym};
+use goblin::elf32::section_header::SHT_GROUP;
+use goblin::strtab::Strtab;
 use regex::Regex;
 use scroll::ctx::IntoCtx;
+use scroll::{Pread, Pwrite};
 use std::collections::HashMap;
+
+const GRP_COMDAT: u32 = 1; // Per https://docs.oracle.com/cd/E23824_01/html/819-0690/chapter7-26.html
 
 pub fn localize_elf_symbols(data: Vec<u8>, keep_regexes: &[Regex]) -> Result<Vec<u8>> {
     let elf = Elf::parse(&data)?;
@@ -24,7 +29,7 @@ pub fn localize_elf_symbols(data: Vec<u8>, keep_regexes: &[Regex]) -> Result<Vec
     let new_symtabs = symtab::localize_elf_symbols(&elf, ctx, &data, keep_regexes)?;
     let new_relocs = reloc::process_elf_relocations(&elf, ctx, &data, &new_symtabs);
 
-    patch_elf(
+    patch_new_elf_symbols(
         elf.header,
         section_headers,
         ctx,
@@ -34,7 +39,7 @@ pub fn localize_elf_symbols(data: Vec<u8>, keep_regexes: &[Regex]) -> Result<Vec
     )
 }
 
-fn patch_elf(
+fn patch_new_elf_symbols(
     elf_header: Header,
     section_headers: SectionHeaders,
     ctx: Ctx,
@@ -77,6 +82,83 @@ fn patch_elf(
 
         let offset = shoff + sh_idx * header_size;
         header.into_ctx(&mut data[offset..], ctx);
+    }
+
+    Ok(data)
+}
+
+pub fn demote_comdat_groups(mut data: Vec<u8>, keep_regexes: &[Regex]) -> Result<Vec<u8>> {
+    let elf = Elf::parse(&data)?;
+    let container = elf.header.container()?;
+    let endianness = elf.header.endianness()?;
+    let section_headers = elf.section_headers;
+    let ctx = Ctx::new(container, endianness);
+
+    'next_section: for header in section_headers.iter() {
+        if header.sh_type != SHT_GROUP || header.sh_flags != 0 {
+            continue;
+        }
+
+        let group_range = header.file_range();
+        let group_data = &data[group_range.start..group_range.end];
+        if group_data.len() < 4 {
+            continue; // Not Supposed To Happen, but can't be too careful with wild ELFs...
+        }
+        let group_flags: u32 = group_data.pread_with(0, endianness).unwrap();
+        if group_flags & GRP_COMDAT == 0 {
+            continue;
+        }
+        if group_flags != GRP_COMDAT {
+            // It's probably safe to unset just GRP_COMDAT, but I can't rule out that someone
+            // will create a new flag that _only_ makes sense alongside GRP_COMDAT,
+            // so out of an abundance of caution let's reject unknown group flags
+            return Err(eyre!(
+                "COMDAT section group also contains unknown flags ({}), refusing to continue",
+                group_flags
+            ));
+        }
+
+        let symtab_idx = header.sh_link as usize;
+        let sym_idx = header.sh_info as usize;
+
+        let symtab = match section_headers.get(symtab_idx) {
+            Some(symtab) => symtab,
+            None => {
+                return Err(eyre!(
+                    "Section group references invalid symbol table index: {}",
+                    symtab_idx
+                ));
+            }
+        };
+        let symtab_range = symtab.file_range();
+        let symtab_data = &data[symtab_range.start..symtab_range.end];
+        let sym_size = sym_size(&ctx);
+        let name_sym: Sym = symtab_data.pread_with(sym_idx * sym_size, ctx).unwrap();
+        let strtab_idx = symtab.sh_link as usize;
+
+        let strtab = if strtab_idx >= section_headers.len() {
+            return Err(eyre!(
+                "Section group symbol references invalid string table index: {}",
+                strtab_idx
+            ));
+        } else {
+            let shdr = &section_headers[strtab_idx];
+            shdr.check_size(data.len())?;
+            Strtab::parse(&data, shdr.sh_offset as usize, shdr.sh_size as usize, 0x0)
+        }?;
+
+        if let Some(Ok(name)) = strtab.get(name_sym.st_name) {
+            for regex in keep_regexes {
+                if regex.is_match(name) {
+                    continue 'next_section;
+                }
+            }
+        } else {
+            continue 'next_section;
+        }
+
+        let demoted_flags = group_flags & !GRP_COMDAT;
+        data.pwrite_with(demoted_flags, group_range.start, endianness)?;
     }
 
     Ok(data)
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ struct Opt {
     #[structopt(short, long, number_of_values = 1)]
     keep_symbols: Vec<String>,
 
+    /// Accepts regexes of the GRP_COMDAT section groups to keep, the rest becomes regular groups
+    #[structopt(long, number_of_values = 1)]
+    keep_comdat_sections: Vec<String>,
+
     /// Input object
     #[structopt(name = "input", parse(from_os_str))]
     input: PathBuf,
@@ -25,11 +29,16 @@ struct Opt {
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
-    if opt.keep_symbols.is_empty() {
-        return Err(eyre!("No symbol filter specified"));
+    if opt.keep_symbols.is_empty() && opt.keep_comdat_sections.is_empty() {
+        return Err(eyre!("No action specified"));
     }
     let keep_regexes = opt
         .keep_symbols
+        .into_iter()
+        .map(|r| Regex::new(&r))
+        .collect::<Result<Vec<_>, _>>()?;
+    let keep_comdat_regexes = opt
+        .keep_comdat_sections
         .into_iter()
         .map(|r| Regex::new(&r))
         .collect::<Result<Vec<_>, _>>()?;
@@ -42,7 +51,16 @@ fn main() -> Result<()> {
         .and_then(|hint_bytes_slice| hint_bytes_slice.try_into().ok());
     let new_data = if let Some(hint_bytes) = hint_bytes {
         match peek_bytes(hint_bytes)? {
-            Hint::Elf(_) => elf::localize_elf_symbols(data, &keep_regexes)?,
+            Hint::Elf(_) => {
+                let mut patched_data = data;
+                if !keep_regexes.is_empty() {
+                    patched_data = elf::localize_elf_symbols(patched_data, &keep_regexes)?
+                }
+                if !keep_comdat_regexes.is_empty() {
+                    patched_data = elf::demote_comdat_groups(patched_data, &keep_comdat_regexes)?
+                }
+                patched_data
+            }
             Hint::Mach(_) | Hint::MachFat(_) => return Err(eyre!("Cannot handle mach objects")),
             Hint::PE => return Err(eyre!("Cannot handle PE objects")),
             _ => return Err(eyre!("Unknown input file type")),
