d5a22f9b3a6eeec25f4c45919b3ea5fe28380586
tux3 <barrdetwix@gmail.com>
2021-06-28 20:16:56 +0200

    Support SHT_SYMTAB_SHNDX when localizing symbols

--- src/elf.rs
+++ src/elf.rs
@@ -3,7 +3,7 @@ mod symtab;
 
 use crate::elf::reloc::{rel_size, ElfRelocationUpdate};
 use crate::elf::symtab::{sym_size, ElfSymbolTableUpdate};
-use eyre::{eyre, Result};
+use eyre::{bail, eyre, Result};
 use goblin::container::{Container, Ctx};
 use goblin::elf::section_header::{
     section_header32, section_header64, SHT_GNU_HASH, SHT_GNU_VERSYM, SHT_HASH, SHT_NULL, SHT_RELA,
@@ -53,14 +53,39 @@ fn patch_new_elf_symbols(
         Container::Little => section_header32::SIZEOF_SHDR,
         Container::Big => section_header64::SIZEOF_SHDR,
     };
+    let mut shndx_to_symtab_map = HashMap::<usize, HashMap<usize, usize>>::new();
+
+    for (sh_idx, header) in section_headers.iter().enumerate() {
+        if header.sh_type == SHT_SYMTAB_SHNDX {
+            if header.sh_info != 0 {
+                bail!("SYMTAB_SHNDX ELF section has invalid non-zero sh_info");
+            }
+            match new_symtabs.get_mut(&(header.sh_link as usize)) {
+                None => bail!("SYMTAB_SHNDX ELF section references invalid symtab in sh_link"),
+                Some(symtab) => {
+                    shndx_to_symtab_map.insert(sh_idx, std::mem::take(&mut symtab.sym_idx_map))
+                }
+            };
+        }
+    }
 
     for (sh_idx, mut header) in section_headers.into_iter().enumerate() {
         // We don't implement hash sections, but they're just an optimization. Discard them.
         if header.sh_type == SHT_HASH || header.sh_type == SHT_GNU_HASH {
             header.sh_type = SHT_NULL;
         } else if header.sh_type == SHT_SYMTAB_SHNDX {
-            // We *could* handle those by just reordering the entries
-            return Err(eyre!("Cannot handle SYMTAB_SHNDX ELF sections"));
+            // SYMTAB_SHNDX entries match the symtab entry order 1:1, so we just apply the same swaps
+            let symtab_idx_map = shndx_to_symtab_map.get(&sh_idx).unwrap();
+            let shndx_range = header.file_range();
+            let shndx_data = &mut data[shndx_range.start..shndx_range.end];
+            for (old_idx, new_idx) in symtab_idx_map {
+                if old_idx < new_idx {
+                    let old_entry: u32 = shndx_data.pread_with(old_idx * 4, ctx.le).unwrap();
+                    let new_entry: u32 = shndx_data.pread_with(new_idx * 4, ctx.le).unwrap();
+                    shndx_data.pwrite_with(new_entry, old_idx * 4, ctx.le)?;
+                    shndx_data.pwrite_with(old_entry, new_idx * 4, ctx.le)?;
+                }
+            }
         } else if header.sh_type == SHT_GNU_VERSYM {
             // We could handle those by reordering the entries, but it's not always enough
             // If we're trying to localize a versioned symbol, we'd need to modify other sections
